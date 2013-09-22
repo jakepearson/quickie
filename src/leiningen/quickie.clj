8cf97beda512207d6ced2676208b590115b7295b
Jake Pearson <jpearson@rallydev.com>
2013-09-22 14:44:42 -0600

    Test selector may be passed in as a command line argument (fixes #2)

--- src/leiningen/quickie.clj
+++ src/leiningen/quickie.clj
@@ -7,20 +7,21 @@
 (defn default-pattern [project]
   (let [name (or (:name project)
                  (:group project))]
-    (re-pattern (str ".*" name ".*"))))
+    (re-pattern (str name ".*test"))))
 
-(defn test-matcher [parameters project]
-  (if-let [matcher (:test-matcher project)]
-    (assoc parameters :test-matcher matcher)
-    (assoc parameters :test-matcher (default-pattern project))))
+(defn test-matcher [project args]
+  (cond
+   (> (count args) 0)      (re-pattern (first args))
+   (:test-matcher project) (:test-matcher project)
+   :else                   (default-pattern project)))
  
 (defn quickie
   "Automatically run tests when clj files change"
   [project & args]
   (eval/eval-in-project 
-    (update-in project [:dependencies] conj ['quickie "0.2.3"])
+    (update-in project [:dependencies] conj ['quickie "0.2.4"])
     (let [parameters (-> {}
                          (paths project)
-                         (test-matcher project))]
+                         (assoc :test-matcher (test-matcher project args)))]
       `(quickie.autotest/run ~parameters))
     `(require 'quickie.autotest)))
