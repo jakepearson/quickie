eef8242ec31147d80ecb2d26c12742f706969654
jakepearson <jpearson@rallydev.com>
2014-10-10 09:50:35 -0600

    Added default ssh key id

--- project.clj
+++ project.clj
@@ -1,7 +1,8 @@
-(defproject quickie "0.3.0"
+(defproject quickie "0.3.1"
   :description "Automatically run tests when clj files change"
   :url "http://github.com/jakepearson/quickie"
   :license {:name "Eclipse Public License" :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :signing {:gpg-key "37634C19"}
   :eval-in-leiningen true
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [midje "1.5.1"]
