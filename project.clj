158d07de27bdc5b764eda98506cc182c3e4b8a78
Mike Holm, Will Beard and Jake Pearson <mholm@rallydev.com>
2014-10-13 14:12:39 +0000

    A better version bump

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject quickie "0.3.3"
+(defproject quickie "0.3.4"
   :description "Automatically run tests when clj files change"
   :url "http://github.com/jakepearson/quickie"
   :license {:name "Eclipse Public License" :url "http://www.eclipse.org/legal/epl-v10.html"}
